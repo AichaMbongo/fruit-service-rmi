79da19611a8d302e503015bf6df874db1bc1efba
Aicha Mbongo <azindamoyen@strathmore.edu>
2025-06-12 17:00:49 +0300

    feat: add UI interface for the fruit server system this is a demo, it is not yet done

--- tasks/AddFruitPrice.java
+++ tasks/AddFruitPrice.java
@@ -1,1 +1,26 @@
-// class AddFruitPrice implements Task<String>
+import java.io.Serializable;
+
+public class AddFruitPrice implements Task<String>, Serializable {
+    private String fruitName;
+    private double price;
+
+    public AddFruitPrice(String fruitName, double price) {
+        this.fruitName = fruitName;
+        this.price = price;
+    }
+
+    @Override
+    public String execute() {
+        // Here you would add logic to store the fruit and price, e.g., in a database or map.
+        // For demonstration, we'll just return a confirmation string.
+        return "Added " + fruitName + " with price $" + price;
+    }
+
+    public String getFruitName() {
+        return fruitName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
