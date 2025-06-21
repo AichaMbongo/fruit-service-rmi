a849f7bc31cd3a0cb294548b4dea7f5d020c9f42
Aicha Mbongo <azindamoyen@strathmore.edu>
2025-06-21 17:21:08 +0300

    fix: change code to generic RMI task interface for compute engine
    Defines a serializable, generic Task<T> interface
    to be implemented by all fruit operation tasks.
    
    issue: #1

--- src/main/java/com/example/fruitservice/rmi/Task.java
+++ src/main/java/com/example/fruitservice/rmi/Task.java
@@ -2,8 +2,24 @@ package com.example.fruitservice.rmi;
 
 import java.io.Serializable;
 
+<<<<<<< Updated upstream
 public interface Task<T> extends Serializable {
     T execute();
 }
 // This interface defines a Task that can be executed remotely via RMI.
 // It extends Serializable to allow the task to be sent over the network.
+=======
+/**
+ * Represents a unit of work that can be executed remotely by a Compute engine.
+ * 
+ * @param <T> The result type returned by this task.
+ */
+public interface Task<T> extends Serializable {
+    /**
+     * Executes the logic of the task and returns the result.
+     *
+     * @return The result of the task execution.
+     */
+    T execute();
+}
+>>>>>>> Stashed changes
