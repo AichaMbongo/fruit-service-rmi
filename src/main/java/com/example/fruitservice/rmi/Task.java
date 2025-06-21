c45e512cca1f098f8caed7cfd6e2a3e57b9b32f9
Aicha Mbongo <azindamoyen@strathmore.edu>
2025-06-21 13:34:32 +0300

    fix: Change code in Task.java
    changed code to add serializable interface to Task.java
    
    issue: #1

--- src/main/java/com/example/fruitservice/rmi/Task.java
+++ src/main/java/com/example/fruitservice/rmi/Task.java
@@ -1,4 +1,9 @@
 package com.example.fruitservice.rmi;
 
-public class Task<T> {
+import java.io.Serializable;
+
+public interface Task<T> extends Serializable {
+    T execute();
 }
+// This interface defines a Task that can be executed remotely via RMI.
+// It extends Serializable to allow the task to be sent over the network.
